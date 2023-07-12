9429d4c8b538bcf9804d849ba09572486e063efb
신희성 <vvwv3092@gmail.com>
2023-07-12 15:21:28 +0900

    Add userInfoContext

--- packages/context/src/App.tsx
+++ packages/context/src/App.tsx
@@ -1,5 +1,20 @@
+import { createContext, useState } from "react";
+
+interface IUserInfo {
+  name?: string;
+  age?: number;
+}
+
+const userInfoContext = createContext({});
+
 function App() {
-  return <></>;
+  const [user, setUser] = useState<IUserInfo>({});
+
+  return (
+    <userInfoContext.Provider
+      value={{ user, setUser }}
+    ></userInfoContext.Provider>
+  );
 }
 
 export default App;
