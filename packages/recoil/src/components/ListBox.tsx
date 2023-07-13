612a65033b8ffcf6357960136571d63c9e934e10
신희성 <vvwv3092@gmail.com>
2023-07-13 12:26:42 +0900

    Add ListBox-text-rendering

--- packages/recoil/src/components/ListBox.tsx
+++ packages/recoil/src/components/ListBox.tsx
@@ -1,7 +1,23 @@
+import { textListSelector } from "../atoms";
+import { useRecoilValue } from "recoil";
+
 export default function ListBox() {
+  const textList = useRecoilValue(textListSelector);
+
+  console.log(textList);
+
   return (
     <div>
       <h3>ListBox</h3>
+      {textList[0] ? (
+        <ul>
+          {textList.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      ) : (
+        <div>아직 작성된 자료가 없어요!</div>
+      )}
     </div>
   );
 }
