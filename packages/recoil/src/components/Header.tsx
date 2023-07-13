3d1c125b3fa2dab70bec334e4c50d0f48981b99d
신희성 <vvwv3092@gmail.com>
2023-07-13 12:27:30 +0900

    Add Header-category-selector

--- packages/recoil/src/components/Header.tsx
+++ packages/recoil/src/components/Header.tsx
@@ -1,7 +1,23 @@
+import { useRecoilState } from "recoil";
+import { categoryState } from "../atoms";
+import React from "react";
+
 export default function Header() {
+  const [category, setCategory] = useRecoilState(categoryState);
+
   return (
     <div>
       <h2>Header</h2>
+      <select
+        value={category}
+        onChange={(e: React.FormEvent<HTMLSelectElement>) => {
+          return setCategory(e.currentTarget.value as any);
+        }}
+      >
+        <option value="ONE">ONE</option>
+        <option value="TWO">TWO</option>
+        <option value="THREE">THREE</option>
+      </select>
     </div>
   );
 }
